d0fe38ee716116da23883c9c5c3fd1647627ebe3
Ashok K <ashok.7406625107@gmail.com>
2026-07-28 21:38:28 +0530

    Add Playwright automation tests

---
+++ tests/Open_Page.spec.ts
@@ -0,0 +1,19 @@
+import {test, expect} from '@playwright/test';
+
+test("Verify page title", async ({page}) => { 
+    await page.goto("https://www.google.com");
+
+
+    let title:string = await page.title();
+    console.log("title = ", title)
+    await expect(page).toHaveTitle("Google");
+
+
+}
+
+
+
+
+
+
+)
