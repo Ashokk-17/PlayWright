8620bd391bd7841f6a2ce866ebc916935f9733e1
Ashok K <ashokk@admins-MacBook-Pro.local>
2026-08-01 00:27:01 +0530

    again

---
+++ tests/chapter_03_Xpaths/8_dynamic_elements.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect, Locator } from "@playwright/test";
+
+test("Verify the dynamic", async ({ page }) => {
+
+    await page.goto("https://testautomationpracticeblogspot.com/");
+
+ await page.waitForTimeout(5000);
+
+
+
+
+
+
+
+
+
+
+});
