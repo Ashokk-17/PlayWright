2c10b494b09c8e37234fa399736d79e945ec7d17
Ashok K <ashok.7406625107@gmail.com>
2026-08-01 11:24:20 +0530

    again

--- tests/chapter_03_Xpaths/8_dynamic_elements.spec.ts
+++ tests/chapter_03_Xpaths/8_dynamic_elements.spec.ts
@@ -1,18 +1,72 @@
 import { test, expect, Locator } from "@playwright/test";
 
+
+
+/*
+// Using Xpath to locate dynamic elements on the page
+
+
 test("Verify the dynamic", async ({ page }) => {
 
-    await page.goto("https://testautomationpracticeblogspot.com/");
+    await page.goto("https://testautomationpractice.blogspot.com/");
 
  await page.waitForTimeout(5000);
 
+//loop to click the button 5 times
+
+for (let i=1; i<=5; i++){
+
+let button:Locator = page.locator("(//button[@name='start' or @name='stop'])");
+await button.click();
+
+await page.waitForTimeout(2000);
+
+}
+
+});
+*/
 
 
+/*
+//Using CSS selectors to locate dynamic elements on the page
+
+test("Verify the dynamic", async ({ page }) => {
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+ await page.waitForTimeout(5000);
+
+//loop to click the button 5 times
+
+for (let i=1; i<=5; i++){
+
+let button:Locator = page.locator("button[name='start'], button[name='stop']");
+await button.click();
+
+await page.waitForTimeout(2000);
+
+}
+
+});
+*/
 
 
+//Using Playwright locators to locate dynamic elements on the page
 
+test("Verify the dynamic", async ({ page }) => {
+    
+    await page.goto("https://testautomationpractice.blogspot.com/");
+        await page.waitForTimeout(5000);
 
+//loop to click the button 5 times
 
+for (let i=1; i<=5; i++){
 
+let button:Locator = page.getByRole('button', { name: /start|stop/i });
+await button.click();
+
+await page.waitForTimeout(2000);
+
+}
 
 });
