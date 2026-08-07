e0056293dfb6bdad9eed21d0cc5f22a969f3244e
Ashok K <ashokk@admins-MacBook-Pro.local>
2026-08-07 15:30:00 +0530

    he

---
+++ tests/chapter_05_Dropdown_SingleSelect/14_Duplicated_Dropdown.spec.ts
@@ -0,0 +1,34 @@
+import {test, expect } from '@playwright/test';
+
+test("Sorted dropdown", async ({page}) => {
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
+const dropdown_options = page.locator("#animals > option");
+console.log(await dropdown_options.allTextContents());
+const option_text : string[] = (await dropdown_options.allTextContents()).map((text) => text.trim());
+
+
+const myset = new Set<string[]>();
+const duplicates = string[] = [];
+
+for (text of option_text) {
+    if (myset.has(text)) {
+        duplicates.push(text);
+    } else {
+        myset.add(text);
+    }
+
+}
+
+console.log("Duplicate values are : ", duplicates);
+
+if (duplicates.length > 0) {
+    console.log("Duplicate values are present in the dropdown");
+} else {
+    console.log("No duplicate values are present in the dropdown");
+}
+
+expect(duplicates.length).toBe(0);
+
+}
+);
