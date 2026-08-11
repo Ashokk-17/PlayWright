b6866d1eb795b8259325c2aaac645229c254e9b1
Ashok K <ashok.7406625107@gmail.com>
2026-08-11 20:13:02 +0530

    static again

--- tests/chapter_09_Table_Static_Dynamic/18_statictable.spec.ts
+++ tests/chapter_09_Table_Static_Dynamic/18_statictable.spec.ts
@@ -30,6 +30,80 @@ const columns: Locator = page.locator("table[name = 'BookTable'] tbody tr th");
 
 await expect(columns).toHaveCount(4);
 
+//Read all data of 2nd row index 2 means 3rd row includng header
+
+const secondrowcells: Locator = rows.nth(2).locator('td');
+let secondrowtexts: string[] = await secondrowcells.allInnerTexts();
+
+console.log("2nd row texts are :", secondrowtexts);   //[ 'Learn Java', 'Mukesh', 'Java', '500' ]
+
+expect(secondrowtexts).toEqual(['Learn Java', 'Mukesh', 'Java', '500']);
+
+console.log("printing 2nd row data.....")
+
+for (let text of secondrowtexts){
+    console.log(text);
+}
+
+
+//Reading all data from table except header
+
+console.log("Printing all data of the table...");
+
+const allrowdata = await rows.all();
+console.log(" Bookname      Author      Subject     Price");
+
+for (let row of allrowdata.slice(1)){
+    const cols = await row.locator('td').allInnerTexts();
+    console.log(cols.join('\t'));
+
+}
+
+
+//Printing book names where author is mukesh 
+
+console.log("Books written by mukesh are ........");
+
+
+const mukeshBooks : string[] = [];
+
+for (let row of allrowdata.slice(1)){
+
+    const cells = await row.locator('td').allInnerTexts();
+    const author = cells[1];
+    const book = cells[0];
+
+
+    if(author === 'Mukesh'){
+        console.log(`${author} \t ${book}`);
+        mukeshBooks.push(book);
+    }
+
+}
+
+expect (mukeshBooks).toHaveLength(2);
+
+
+
+//Calculate the totalprice of all the book 
+
+let totalprice: number = 0;
+
+for(let row of allrowdata.slice(1)){
+
+    const cells = await row.locator('td').allInnerTexts();
+    const price = cells[3];
+
+    totalprice = totalprice + parseInt(price);
+
+}
+
+console.log("Total price = ", totalprice);
+
+expect(totalprice).toBe(7100);
+
+
+
 
 
 
