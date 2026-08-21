c8ff665b286da91e2e619399fa2c2621c4d4edb1
Ashok K <ashok.7406625107@gmail.com>
2026-08-21 09:27:08 +0530

    f

--- tests/chapter_15_File_upload_download/34_fileupload.spec.ts
+++ tests/chapter_15_File_upload_download/34_fileupload.spec.ts
@@ -1,21 +1,55 @@
 import {test, expect} from '@playwright/test';
 
-test("Keyboard actions", async ({page})=> {
+test("File upload single", async ({page})=> {
 
  await page.goto("https://testautomationpractice.blogspot.com/");
 
 
- const singlfile = page.locator("#singleFileInput").setInputFiles('../Upload_files/file2.txt');
- const singlupload = page.locator("button:has-text('Upload Single File')").click();
+
+ // Single file Upload
+
+ const singlfile = await page.locator("#singleFileInput").setInputFiles('../Upload_files/file2.txt');
+ const singlupload = await page.locator("button:has-text('Upload Single File')").click();
+ //await page.getByRole('button', { name: 'Upload Single File' }).click();
 
  await page.waitForTimeout(5000);
 
  
+const msg = await page.locator("#singleFileStatus").textContent();
+expect(msg).toContain('file2.txt');
+
+console.log("File upload is successful",msg);
+
+
+ await page.waitForTimeout(5000);
+
+})
+
+
+//multiple file upload
+
+test.only("File upload multiple", async ({page})=> {
+
+ await page.goto("https://testautomationpractice.blogspot.com/");
 
 
 
+ // Multiple file Upload
+
+ const Multiplefile = await page.locator("#multipleFilesInput").setInputFiles(['../Upload_files/file2.txt','../Upload_files/file1.png']);
+ const Multiupload = await page.locator("button:has-text('Upload Multiple Files')").click();
+ //await page.getByRole('button', { name: 'Upload Single Files' }).click();
+
+ await page.waitForTimeout(5000);
+
+ 
+const msg1 = await page.locator("#multipleFileStatus").textContent();
+ expect(msg1).toContain('file2.txt');
+ expect(msg1).toContain('file1.png');
+
+console.log("File upload is successful",msg1);
 
 
-
+ await page.waitForTimeout(5000);
 
 })
